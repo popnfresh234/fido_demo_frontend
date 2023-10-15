6a2b4ccf3745003347468be005b2ce266e1c7183
Alex Holliday <ajhollid@gmail.com>
2023-10-15 02:47:45 -0700

    thing

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from "@angular/router";
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/auth.interceptor';
 import routeConfig from "./app/routes";
 
 bootstrapApplication(AppComponent,
   {
     providers:
       [provideProtractorTestingSupport(),
-      provideRouter(routeConfig)
+      provideRouter(routeConfig),
+      provideHttpClient(withInterceptors([authInterceptor]))
       ]
   })
   .catch(err => console.error(err));
